fd07f2245cc4197c9e1ed0d95eaeaeac4e7706c5
msmukhlis <msmukhlisss@gmail.com>
2024-07-24 13:02:30 +0500

    Commit message describing your changes

--- src/pages/About/about.jsx
+++ src/pages/About/about.jsx
@@ -1,9 +1,23 @@
+import styles from "../About/about.module.css"
 
 export const About =()=>{
 
     return(
-        <div className="about">
-            About page
+        <div className={styles.container_about}>
+            <div className={styles.pic_info}>
+                <div className={styles.left_side}>
+                    <div className={styles.image_layout}>
+
+                    </div>
+                    <div className={styles.client_panel}>
+
+                    </div>
+                </div>
+                <div className={styles.title_details}>
+
+
+                </div>
+            </div>
         </div>
     )
 }
