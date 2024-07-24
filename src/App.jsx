c0c30574b7b769bd859ec187ba28805cbf50417a
msmukhlis <msmukhlisss@gmail.com>
2024-07-24 10:01:33 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <Routes>
+    <Route path="/">
+      {/* <Route index element={<Home />} />
+      <Route path="blogs" element={<Blogs />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="*" element={<NoPage />} /> */}
+    </Route>
+  </Routes>
+  )
+}
+
+export default App
